cba550857ec268a9484c8267e989c4b14970b139
Zettz <calderonlorenzo026@gmail.com>
2026-03-09 14:24:30 -0700

    started a35 code

--- main.cpp
+++ main.cpp
@@ -10,12 +10,18 @@ int main()
     int total = 0;
 
     // TODO: seed the random number generator with time(0)
-
+    srand(time(0));
     // TODO: generate 3 random numbers between 0 and 99
+    num1 = rand() % 100;
+    num2 = rand() % 100;
+    num3 = rand() % 100;
 
     // TODO: compute the total summation
+    total = num1 + num2 + num3;
 
     // TODO: print the 3 random numbers on the first line
+    cout << fixed << setprecision(2);
+    cout << "The three random numbers are: " << num1 << ", " << num2 << ", and " << num3 << endl;
 
     // TODO: print the total and average (integer division) on the second line
 
